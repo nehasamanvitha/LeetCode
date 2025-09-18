8ad35a322b8925160c8862ba33e5ac52f16564bf
nehasamanvitha <nehasamanvitha@gmail.com>
2025-09-18 17:34:03 +0530

    Time: 0 ms (100%), Space: 12.7 MB (25.09%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -5,11 +5,5 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        seen=set(nums1)
-        nums=set()
-        for num in nums2:
-            if num in seen:
-                nums.add(num)
-        return list(nums)
-        
+        return list(set(nums1) & set(nums2))
         
