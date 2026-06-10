499948b50e756be30e7d48643a34afdc56dde38a
nehasamanvitha <nehasamanvitha@gmail.com>
2026-06-10 10:34:59 +0530

    Time: 21 ms (69.91%), Space: 13.4 MB (8.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: int
         """
         left=0
-        ans=0
+        max_len=0
         seen=set()
-        for right in range(0,len(s)):
+        for right in range(len(s)):
             while s[right] in seen:
                 seen.remove(s[left])
                 left+=1
+         
             seen.add(s[right])
-            ans=max(ans,right-left+1)
-        return ans
-
-        
+            max_len=max(max_len,right-left+1)
+        return max_len
