5729ddbcf796e701035b7582d8179132af706fdf
nehasamanvitha <nehasamanvitha@gmail.com>
2026-08-01 22:54:39 +0530

    Time: 16 ms (60.39%), Space: 12.5 MB (51.76%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minOperations(self, s):
+        count1 = 0
+        count2 = 0
+
+        for i in range(len(s)):
+            # Expected characters
+            expected1 = '0' if i % 2 == 0 else '1'
+            expected2 = '1' if i % 2 == 0 else '0'
+
+            if s[i] != expected1:
+                count1 += 1
+
+            if s[i] != expected2:
+                count2 += 1
+
+        return min(count1, count2)
+        
