dace8f5eced1d2c2b83a34f7cc91874ae5ded18b
nehasamanvitha <nehasamanvitha@gmail.com>
2026-08-06 22:32:57 +0530

    Time: 19 ms (90.58%), Space: 12.8 MB (46.93%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) > len(s2):
+            return False
+        need = [0] * 26
+        window = [0] * 26
+        k = len(s1)
+        for i in range(k):
+            need[ord(s1[i]) - ord('a')] += 1
+            window[ord(s2[i]) - ord('a')] += 1
+        if need == window:
+            return True
+        left = 0
+        for right in range(k, len(s2)):
+            window[ord(s2[right]) - ord('a')] += 1
+            window[ord(s2[left]) - ord('a')] -= 1
+            left += 1
+            if need == window:
+                return True
+        return False
