7d71553340131dd358595615f9dded1cc9148ccc
Neha Samanvitha Valiveti <nehasamanvitha@gmail.com>
2026-08-12 22:27:23 +0530

    Time: 4 ms (4.16%), Space: 12.4 MB (27.58%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,16 +6,13 @@ class Solution(object):
         slow = dummy
         fast = dummy
 
-        # Move fast n+1 steps ahead
         for _ in range(n + 1):
             fast = fast.next
 
-        # Move both until fast reaches the end
         while fast:
             slow = slow.next
             fast = fast.next
 
-        # Remove the target node
         slow.next = slow.next.next
 
         return dummy.next
